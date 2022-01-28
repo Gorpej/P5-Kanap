c562568b41d91d035b4a4f882b471d95fcfa1c95
Jeremy <aviathur@gmail.com>
2022-01-28 17:40:11 -0500

    amelioration du code kanap accueil

--- front/js/script.js
+++ front/js/script.js
@@ -1,6 +1,28 @@
 const linkItem = document.querySelector('#items');
 
+const showItems = async function () {
+  let response = await fetch("http://localhost:3000/api/products/")
+      if (response.ok) {
+       let data = await response.json()
+        console.log(data)
+        for (product of data) {
+          linkItem.innerHTML +=
+            `<a href="./product.html?id=${product._id}">
+                <article>
+                  <img src="${product.imageUrl}" alt="${product.altTxt}">
+                  <h3 class="productName">${product.name}</h3>
+                  <p class="productDescription">${product.description}</p>
+                </article>
+              </a>`;
+        }
+      } else{
+        console0eror('retour du serveur ; ', response.status)
+      }
+    }
+      
+showItems();
 
+/* 
 // Requete API
 async function showItems() {
   const request = await fetch("http://localhost:3000/api/products/",
@@ -13,18 +35,17 @@ async function showItems() {
   } else {
     let data = await request.json();
 
-    for (item of data) {
+    for (product of data) {
       linkItem.innerHTML +=
-        `<a href="./product.html?id=${item._id}">
+        `<a href="./product.html?id=${product._id}">
             <article>
-              <img src="${item.imageUrl}" alt="${item.altTxt}">
-              <h3 class="productName">${item.name}</h3>
-              <p class="productDescription">${item.description}</p>
+              <img src="${product.imageUrl}" alt="${product.altTxt}">
+              <h3 class="productName">${product.name}</h3>
+              <p class="productDescription">${product.description}</p>
             </article>
           </a>`;
-      console.log(item);
     }
   }
 }
 
-showItems();
+showItems(); */
