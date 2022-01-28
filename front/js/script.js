fa7a23065db0220025cf45cef18a552848113ed1
Jeremy <aviathur@gmail.com>
2022-01-28 13:52:58 -0500

    integration kanap page accueil

--- front/js/script.js
+++ front/js/script.js
@@ -1,57 +1,30 @@
-// let linkItem = document.querySelector('#items');
+const linkItem = document.querySelector('#items');
 
 
 // Requete API
-async function showItems(){
- const request = await fetch("http://localhost:3000/api/products/", 
-  {
-    method: 'GET'
-  });
+async function showItems() {
+  const request = await fetch("http://localhost:3000/api/products/",
+    {
+      method: 'GET'
+    });
   if (!request.ok) {
-    alert('un probleme est survenu');
+    console.log('Un probleme est survenu');
+    
   } else {
     let data = await request.json();
-    console.log(data);
-    
-    for (const item of data){
-      document.querySelector('#items').innerHTML =
+
+    for (item of data) {
+      linkItem.innerHTML +=
         `<a href="./product.html?id=${item._id}">
-          <article>
-            <img src="${item.imageUrl}" alt="${item.altTxt}">
-            <h3 class="productName">${item.name}</h3>
-            <p class="productDescription">${item.description}</p>
-          </article>
-        </a>`;
-     console.log(item);
+            <article>
+              <img src="${item.imageUrl}" alt="${item.altTxt}">
+              <h3 class="productName">${item.name}</h3>
+              <p class="productDescription">${item.description}</p>
+            </article>
+          </a>`;
+      console.log(item);
     }
   }
 }
+
 showItems();
-
-// Requéte API
-
-/* let items;
-
-const fetchItems = async () => {
-  items = await fetch("http://localhost:3000/api/products/")
-    .then((res) => res.json());
-};
-
-const showItems = async () => {
-  await fetchItems();
-  linkItem.innerHTML = (items
-      .map(item => (
-        `<a href="./product.html?id=${item._id}">
-          <article>
-            <img src="${item.imageUrl}" alt="${item.altTxt}">
-            <h3 class="productName">${item.name}</h3>
-            <p class="productDescription">${item.description}</p>
-          </article>
-        </a>`
-      )).join('')
-  );
-};
-
-showItems(); */
-
-
