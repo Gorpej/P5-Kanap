52da7c863053d4e7efab34e01a278157b08828da
Jeremy <aviathur@gmail.com>
2022-02-16 13:59:37 -0500

    Page panier modif

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,26 +1,25 @@
 let cartSelectItem = document.querySelector('#cart__items');
-let totalQuantity = document.querySelector('#totalQuantity');
-let totalPrice = document.querySelector('#totalPrice');
+let selectTotalQuantity = document.querySelector('#totalQuantity');
+let selectTotalPrice = document.querySelector('#totalPrice');
 let cart = JSON.parse(localStorage.getItem('Cart') || []);
-let quantityTotal = [];
-let priceTotal = [];
-// let modifQuantity = document.getElementsByClassName('.itemQuantity');
-// let deleteItem = document.getElementsByClassName('deleteItem');
+let mQuantity = document.getElementsByClassName('itemQuantity');
+let deleteItemSelect = document.getElementsByClassName('deleteItem');
+let quantityTotal = 0;
+let priceTotal = 0;
+let cartItem;
+
 // let firstNameErrorMsg = document.querySelector('#firstNameErrorMsg');
 
 addInsertCart();
-addQuantity(quantityTotal);
-addPrice(priceTotal);
-
-console.log(quantityTotal);
-console.log(priceTotal);
+selectTotalQuantity.innerHTML = quantityTotal;
 
 // récupére le detail du panier du localStorage un par un.
-function addInsertCart() {
+function addInsertCart(cartITem) {
   for (let i = 0; i < cart.length; i++) {
-    quantityTotal.push(cart[i].quantity)
-    apiCall(i); 
-    
+
+    quantityTotal += cart[i].quantity;
+    apiCall(i);
+
   }
 }
 
@@ -30,13 +29,8 @@ async function apiCall(i) {
   if (response.ok) {
     let data = await response.json();
 
-    // multiplication de la quantité récuperée dans le localstorage par le prix récuperé dans l'API
-
-    let resultPrice = cart[i].quantity * data.price;
-    priceTotal.push(resultPrice);
-
     cartSelectItem.innerHTML +=
-      `<article class="cart__item" data-id="${data.id}" data-color="${cart[i].color}">
+      `<article class="cart__item" data-id="${data._id}" data-color="${cart[i].color}">
            <div class="cart__item__img">
              <img src="${data.imageUrl}" alt="${data.altTxt}"></img>
            </div>
@@ -48,8 +42,8 @@ async function apiCall(i) {
              </div>
              <div class="cart__item__content__settings">
                <div class="cart__item__content__settings__quantity">
-                 <p>Qté : ${cart[i].quantity}</p>
-                 <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="0">
+                 <p>Qté : </p>
+                 <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${cart[i].quantity}">
                </div>
                <div class="cart__item__content__settings__delete">
                  <p class="deleteItem">Supprimer</p>
@@ -58,34 +52,56 @@ async function apiCall(i) {
            </div>
        </article>`;
 
+    // multiplication de la quantité récuperée dans le localstorage par le prix récuperé dans l'API
+
+    let resultPrice = cart[i].quantity * data.price;
+    priceTotal += resultPrice;
+    selectTotalPrice.innerHTML = priceTotal;
+
+    function modQuantity() {
+      for (let input of mQuantity) {
+        return input
+}
+        input.addEventListener('change', event => {
+          console.log(event)
+          let articleId = event.target.closest("article").dataset.id;
+          let articleColor = event.target.closest("article").dataset.color;
+          let modifyQuantity = parseInt(input.value);
+
+          if (cart[i].id === articleId && cart[i].color === articleColor) {
+            cart[i].quantity = modifyQuantity;
+            localStorage.setItem('Cart', JSON.stringify(cart));
+
+          }
+        })
+      
+    }
+
+    function deleteItem() {
+      for (let itemDel of deleteItemSelect) {
+        
+        console.log(itemDel)
+        itemDel.addEventListener('click', event => {
+          console.log(event)
+          // let articleId = event.target.closest("article").dataset.id;
+          // let articleColor = event.target.closest("article").dataset.color;
+          //  if (cart[i].id === articleId && cart[i].color === articleColor) {
+
+          //   let test = cart.splice(cart.indexOf(event), 1);
+          // localStorage.setItem('Cart', JSON.stringify(cart));
+          console.log(cart[i].id)
+
+          // }
+        })
+      }
+    }
+    // deleteItem();
+    modQuantity();
+
+
   } else {
     console.error('Un probléme est survenu, retour du serveur: ', response.status)
   }
 }
 
-function addQuantity(array) {
-  const initialValue = 0;
-  const sumWithInitial = array.reduce((previousValue, currentValue) => previousValue + currentValue, initialValue);
-  console.log(sumWithInitial)
-  totalQuantity.innerHTML += `${sumWithInitial}`;
-}
 
-function addPrice(array) {
-  const initialValue = 0;
-  const sumWithInitial = array.reduce((previousValue, currentValue) => previousValue + currentValue, initialValue);
-  console.log(sumWithInitial);
-  totalPrice.innerHTML += `${sumWithInitial}`;
-}
-
-
-// function modQuantity() {
-//   if (modifQuantity.value >= 1 || modifQuantity.value < 100) {
-// localStorage.setItem('Cart', JSON.stringify(cart));
-//   }
-// }
-
-// let test = modifQuantity.closest('value');
-// console.log(test);
-// console.log(modifQuantity);
-
-// modQuantity();
